0596d918a986233b757a30288c3691a990e6d89b
kayczns <144441262+kayczns@users.noreply.github.com>
2023-09-20 18:46:00 +0800

    Update post.dart

--- post.dart
+++ post.dart
@@ -2,20 +2,24 @@ class Post {
   final String profImg;
   final String profName;
   final String postTime;
-  final String profDesc;
+  final String postDesc;
   final String postImg;
-  final int postComments;
-  final int postShares;
-  bool isLiked = false;
+  final dynamic postReaction;
+  final dynamic postColor;
+  final dynamic postComments;
+  final dynamic postShares;
+  bool isLiked;
 
   Post({
     required this.profImg,
     required this.profName,
     required this.postTime,
-    required this.profDesc,
+    required this.postDesc,
     required this.postImg,
+    required this.postReaction,
+    required this.postColor,
     required this.postComments,
     required this.postShares,
-    
-  })
+    required this.isLiked,
+  });
 }
